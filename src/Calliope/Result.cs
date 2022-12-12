67ffeaeb988b04af7fa604c6f3339371d7b93d20
Andrew Morger <asmorger@live.com>
2022-12-11 21:45:29 -0600

    Replaces previous Select logic with LINQ compatible version

--- src/Calliope/Result.cs
+++ src/Calliope/Result.cs
@@ -48,77 +48,51 @@ public record Result<T>
 
     public bool IsError() => this is Failure;
     public bool IsOk() => this is Success;
-
-    public Result<TOutput> Select<TOutput>(Func<T, Result<TOutput>> onSuccess) =>
-        Match(onSuccess.Invoke, e => new Result<TOutput>.Failure(e));
-    
-    public async Task<Result<TOutput>> SelectAsync<TOutput>(Func<T, Task<Result<TOutput>>> onSuccess) =>
-        this switch
-        {
-            Success success => await onSuccess.Invoke(success.Value),
-            Failure failure => new Result<TOutput>.Failure(failure.Error),
-            _ => throw new ArgumentOutOfRangeException()
-        };
 }
 
 public static class ResultExtensions
 {
     public static Result<T> Ok<T>(T value) => new Result<T>.Success(value);
     public static Result<T> Fail<T>(DomainError error) => new Result<T>.Failure(error);
-    
-    public static async Task<Result<TOutput>> SelectAsync<T, TOutput>(this Task<Result<T>> action, 
-        Func<T, Task<Result<TOutput>>> onSuccess)
+   
+    public static Result<TResult> SelectMany<TFirst, TSecond, TResult>(
+        this Result<TFirst> first,
+        Func<TFirst, Result<TSecond>> getSecond,
+        Func<TFirst, TSecond, TResult> getResult)
     {
-        var result = await action;
-        return result switch
-        {
-            Result<T>.Success success => await onSuccess.Invoke(success.Value),
-            Result<T>.Failure failure => new Result<TOutput>.Failure(failure.Error),
-            _ => throw new ArgumentOutOfRangeException()
-        };
+        return first.Match(
+            // First operand has value -> continue to the second operand
+            firstValue => getSecond(firstValue).Match(
+                // Second operand has value -> compose the result from the first and second operands
+                secondValue => Ok(getResult(firstValue, secondValue)),
+    
+                // Second operand is error -> return
+                Fail<TResult> 
+            ),
+    
+            // First operand is error -> return
+            Fail<TResult> 
+        );
     }
     
-    public static async Task<Result<(T, TOutput)>> SelectAndContinueAsync<T, TOutput>(this Task<Result<T>> action, 
-        Func<T, Task<Result<TOutput>>> onSuccess)
+    public static async Task<Result<TResult>> SelectMany<TFirst, TSecond, TResult>(
+        this Task<Result<TFirst>> first,
+        Func<TFirst, Task<Result<TSecond>>> getSecond,
+        Func<TFirst, TSecond, TResult> getResult)
     {
-        var result = await action;
-
-        switch (result)
-        {
-            case Result<T>.Success s:
-                var result2 = await onSuccess.Invoke(s.Value);
-                return result2 switch
-                {
-                    Result<TOutput>.Success success => new Result<(T, TOutput)>.Success((s.Value, success.Value)),
-                    Result<TOutput>.Failure failure => new Result<(T, TOutput)>.Failure(failure.Error),
-                    _ => throw new ArgumentOutOfRangeException()
-                };
-            case Result<T>.Failure f:
-                return new Result<(T, TOutput)>.Failure(f.Error);
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
-    }
+        var firstOption = await first;
     
-    public static async Task<Result<(T, TOutput)>> SelectAndContinueAsync<T, TOutput>(this Task<Result<T>> action, 
-        Func<T, Result<TOutput>> onSuccess)
-    {
-        var result = await action;
-
-        switch (result)
-        {
-            case Result<T>.Success s:
-                var result2 = onSuccess.Invoke(s.Value);
-                return result2 switch
-                {
-                    Result<TOutput>.Success success => new Result<(T, TOutput)>.Success((s.Value, success.Value)),
-                    Result<TOutput>.Failure failure => new Result<(T, TOutput)>.Failure(failure.Error),
-                    _ => throw new ArgumentOutOfRangeException()
-                };
-            case Result<T>.Failure f:
-                return new Result<(T, TOutput)>.Failure(f.Error);
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
+        return await firstOption.Match(
+            async firstValue =>
+            {
+                var secondOption = await getSecond(firstValue);
+    
+                return secondOption.Match(
+                    secondValue => Ok(getResult(firstValue, secondValue)),
+                    Fail<TResult>
+                );
+            },
+            e => Task.FromResult(Fail<TResult>(e))
+        );
     }
 }
